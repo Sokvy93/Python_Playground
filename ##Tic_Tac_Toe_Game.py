525f6a3c41c25a7841c58d2e621f044ddb3f9e75
Victoria Sok <54297772+Sokvy93@users.noreply.github.com>
2019-11-07 22:11:42 -0800

    Update ##Tic_Tac_Toe_Game.py

--- ##Tic_Tac_Toe_Game.py
+++ ##Tic_Tac_Toe_Game.py
@@ -1,7 +1,10 @@
 from IPython.display import clear_output
 
 def display_board(board):
-
+  
+  #to clear the output so it does not show the entire history of all the print funtions or print statements
+  print('\n'*100)
+  
   print(board[7]+'|'+board[8]+'|'+board[9])
   print(board[4]+'|'+board[5]+'|'+board[6])
   print(board[1]+'|'+board[2]+'|'+board[3])
