f9dd0285ff409ee9b47de855450bbf2195f5d4f9
Victoria Sok <54297772+Sokvy93@users.noreply.github.com>
2019-11-08 23:00:44 -0800

    Update ##Tic_Tac_Toe_Game.py

--- ##Tic_Tac_Toe_Game.py
+++ ##Tic_Tac_Toe_Game.py
@@ -6,7 +6,9 @@ def display_board(board):
   print('\n'*100)
   
   print(board[7]+'|'+board[8]+'|'+board[9])
+  
   print(board[4]+'|'+board[5]+'|'+board[6])
+  
   print(board[1]+'|'+board[2]+'|'+board[3])
   
   
